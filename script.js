4815395c0644a5199239929323830ccd39e2d7c7
Lymeng <phatlymeng@gmail.com>
2026-04-06 08:57:53 +0700

    Release/1.0.0 (#4)
    * feat (#1)
    
    * Add File
    
    * feat: implement add-sub
    
    * feat: Multiplication (#2)
    
    * feat: add feature divi (#3)
    
    Co-authored-by: limhongseng18gmail.com <anbschool0022@AnBs-Mac-mini-5.local>
    
    ---------
    
    Co-authored-by: ChhormSann <151742496+Chhormrasann@users.noreply.github.com>
    Co-authored-by: christpor <porkh377@gmail.com>
    Co-authored-by: Seng <163701262+Sengster8888@users.noreply.github.com>
    Co-authored-by: limhongseng18gmail.com <anbschool0022@AnBs-Mac-mini-5.local>

---
+++ script.js
@@ -0,0 +1,101 @@
+let currentOperand = '0';
+let previousOperand = '';
+let operation = undefined;
+
+const currentOperandTextElement = document.getElementById('current-operand');
+const previousOperandTextElement = document.getElementById('previous-operand');
+
+function clearDisplay() {
+    currentOperand = '0';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function deleteLast() {
+    if (currentOperand === '0') return;
+    if (currentOperand.length === 1) {
+        currentOperand = '0';
+    } else {
+        currentOperand = currentOperand.toString().slice(0, -1);
+    }
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    if (currentOperand === '0' && number !== '.') {
+        currentOperand = number.toString();
+    } else {
+        currentOperand = currentOperand.toString() + number.toString();
+    }
+    updateDisplay();
+}
+
+function chooseOperator(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+    updateDisplay();
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+         case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    
+    currentOperand = computation.toString();
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    currentOperandTextElement.innerText = currentOperand;
+    if (operation != null) {
+        previousOperandTextElement.innerText = `${formatNumber(previousOperand)} ${operation}`;
+    } else {
+        previousOperandTextElement.innerText = '';
+    }
+}
+
+function formatNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+        integerDisplay = '';
+    } else {
+        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+    }
+    if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`;
+    } else {
+        return integerDisplay;
+    }
+}
+
+// Initialize display
+updateDisplay();
