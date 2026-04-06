d391ea68fb07a2b1b0ec3429aad3e1e7c7d7bbfc
ChhormSann <bruceleeftsann@gmail.com>
2026-04-06 08:35:56 +0700

    feat: implement add-sub

--- script.js
+++ script.js
@@ -43,7 +43,26 @@ function chooseOperator(op) {
 }
 
 function compute() {
-    // Logic to be implemented by team members
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        default:
+            return;
+    }
+    
+    currentOperand = computation.toString();
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
 }
 
 function updateDisplay() {
